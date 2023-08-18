10b2d08a2de568e2935ee986c6078c6c6b4ae029
dhramenkov <dmitry.hramenkov@jazzteam.org>
2023-08-18 09:33:06 +0300

    Second commit

---
+++ LearnJava/src/ShowMeNewWindowDemo.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class ShowMeNewWindowDemo {
+    public static void main(String[] args){
+        String title = "Message";
+        String text = "Continue learn java";
+        JOptionPane.showMessageDialog(null,text, title, JOptionPane.QUESTION_MESSAGE);
+    }
+}
