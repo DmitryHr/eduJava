45286977edeaa7724fd58a399fc2b93a33d752c8
dhramenkov <dmitry.hramenkov@jazzteam.org>
2023-09-12 09:24:30 +0300

    тестовый коммит

--- LearnJava/src/ConsoleInputDemo.java
+++ LearnJava/src/ConsoleInputDemo.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ConsoleInputDemo {
+import java.util.Scanner;
+
+public class ConsoleInputDemo {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String day, mounth;
+        System.out.println("Какаой сегодня день?");
+        day = input.nextLine();
+        System.out.println("Какой сейчас месяц?");
+        mounth = input.nextLine();
+        String text;
+        text = "Сегодня "+day+ ", месяц - "+ mounth;
+        System.out.println(text);
+    }
 }
--- LearnJava/src/ShowMeWindowDemo.java
+++ LearnJava/src/ShowMeWindowDemo.java
@@ -2,6 +2,11 @@ import javax.swing.JOptionPane;
 
 public class ShowMeWindowDemo {
     public static void main (String[] args){
-        JOptionPane.showMessageDialog(null, "Моя не первая программа!");
+        String title;
+        String text;
+        title = JOptionPane.showInputDialog(null, "Name for window", "Name", JOptionPane.WARNING_MESSAGE);
+        text = JOptionPane.showInputDialog(null, "Text message", "Содержание", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, text, title, JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Моя не первая программа!");
     }
 }
