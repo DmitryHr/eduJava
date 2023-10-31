a4265167c7cc34b629bd91c063655665ba6e6f2c
dhramenkov <dmitry.hramenkov@jazzteam.org>
2023-10-31 09:36:26 +0300

    Доработал класс UsingObjectDemo

---
+++ LearnJava/src/MethodOverloadingApplication.java
@@ -0,0 +1,56 @@
+public class MethodOverloadingApplication {
+public static void main(String[]arg){
+
+    MyClass01 objA, objB;
+    objA = new MyClass01();
+    objB = new MyClass01();
+
+    objA.set(100);
+    objB.set('A');
+
+    System.out.println("Объект objA:");
+    objA.show();
+    objB.set();
+    objB.show();
+    objB.set(200, 'B');
+    System.out.println("Объект после изменения");
+    objB.show("Число", "Сисмвол");
+
+}
+}
+
+class MyClass01{
+    int number;
+    char symbol;
+    void set(int n){
+        number = n;
+    }
+
+    void set(char s){
+        symbol = s;
+    }
+
+    void set(int n, char s){
+        set(n);
+        set(s);
+    }
+
+    void set(){
+        set(0, 'Z');
+    }
+
+    void show(){
+        System.out.println("Значение полей "+number+ " и " + symbol);
+    }
+
+    void show(String txt){
+        System.out.println(txt+"Значение полей "+number+ " и " + symbol);
+    }
+
+    void show(String txt, String txt2){
+        System.out.println(txt+": "+number);
+        System.out.println(txt+": "+symbol);
+    }
+}
+
+
