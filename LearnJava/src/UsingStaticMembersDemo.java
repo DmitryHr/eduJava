ff9c0b276bce29ec002f9493700f24e054278831
dhramenkov <dmitry.hramenkov@jazzteam.org>
2024-01-11 09:13:28 +0300

    Продолжение изучения статических методов

--- LearnJava/src/UsingStaticMembersDemo.java
+++ LearnJava/src/UsingStaticMembersDemo.java
@@ -5,7 +5,7 @@ class MyClass1251 {
 
     System.out.println("Создан объект номер "+count);
     }
-   void show(){
+   static void show(){
         System.out.println("Количество объектов: "+count);
     }
 
@@ -13,17 +13,19 @@ class MyClass1251 {
 
 class UsingStaticMembersDemo {
     public static void main (String [] args){
-        MyClass1251 stMeth = new MyClass1251();
-        stMeth.show();
+//        MyClass1251 stMeth = new MyClass1251();
+//        stMeth.show();
+
+        MyClass1251.show();
 
         MyClass1251 objA = new MyClass1251();
         MyClass1251 objB = new MyClass1251();
         MyClass1251 objC = new MyClass1251();
         MyClass1251 objD = new MyClass1251();
 
-        stMeth.show();
+//        stMeth.show();
 
-//        objB.show();
-//        objC.show();
+        objB.show();
+        objC.show();
     }
 }
